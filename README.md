021fa90994cad3ca7ab3f96beedb6ee6a5cf08d7
Bastien Thuet <bastien.thuet@enseirb-matmeca.fr>
2019-03-28 12:51:56 +0100

    début du radar

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 This is the repositery for the audio's mini-game for ENSEIRB-MATMECA's makers project by THUET Bastien, JOSSE Ivan, BAHOU Anas, ELASSAM Abdelkarim, ? Othmane, JIANG Pierre.
+
+ssh pi@192.168.43.255
+makers2019
+
+to mount pi directory on your machine:
+sshfs pi@192.168.43.225:/home/pi/makers ./   (the directory you want)
--- sound.py
+++ sound.py
@@ -4,8 +4,6 @@ import time
 import pygame
 import os
 
-pause = 1
-letterPause = 3
 A=[0,1]
 B=[1,0,0,0]
 C=[1,0,1,0]
@@ -13,24 +11,35 @@ D=[1,0,0,0]
 E=[0]
 F=[0,0,1,0]
 
-
 def playLetter(L):
   for i in L:
     if i == 0:
-      pygame.mixer.Channel(0).play(ding, maxtime=3000)
+      pygame.mixer.Channel(0).play(court)
       while pygame.mixer.Channel(0).get_busy() == True:
         continue
     elif i == 1:
-      pygame.mixer.Channel(1).play(ding, maxtime=10000)
+      pygame.mixer.Channel(1).play(long)
       while pygame.mixer.Channel(1).get_busy() == True:
         continue
-      time.sleep(pause)
-  time.sleep(letterPause)
-    
-
-
-
         
+    
+def playEE():
+    pygame.mixer.Channel(1).play(EE)
+    vol = 0.6
+    i = 0
+    while pygame.mixer.Channel(1).get_busy() == True:
+        i = i + 1
+        if (i%1000000 == 0) and vol != 0:
+            vol = vol - 0.05
+            pygame.mixer.music.set_volume(vol)
+    i = 0
+    while vol != 0.6:
+        i = i + 1
+        if i%100000 == 0:
+            vol = vol + 0.05
+            pygame.mixer.music.set_volume(vol)
+    
+            
 # initialize
 pygame.mixer.pre_init()
 pygame.mixer.init()
@@ -38,27 +47,39 @@ pygame.init()
 
 # start playing the background music
 pygame.mixer.music.load(os.path.join(os.getcwd(), 'sounds', 'ambiance_underwater.wav'))
-pygame.mixer.music.set_volume(0.3)
+pygame.mixer.music.set_volume(0.6)
 pygame.mixer.music.play(loops=-1)  # loop forever
 
 pygame.display.set_mode()
 
-ding = pygame.mixer.Sound("sounds/radar_ping_lent.wav")
-pygame.mixer.Channel(0).set_volume(0.9)
-pygame.mixer.Channel(1).set_volume(0.5)
+court = pygame.mixer.Sound("sounds/Court.wav")
+long = pygame.mixer.Sound("sounds/Long.wav")
+EE = pygame.mixer.Sound("sounds/EE.wav")
+
+pygame.mixer.Channel(0).set_volume(1.0)
+pygame.mixer.Channel(1).set_volume(1.0)
 pygame.mixer.Channel(0).set_endevent()
 pygame.mixer.Channel(1).set_endevent()
 
-
+ee = 0
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit(); #sys.exit() if sys is imported
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_p:
+            if event.key == pygame.K_a:
+                ee = 0
                 playLetter(A)
-            if event.key == pygame.K_g:
+            if event.key == pygame.K_b:
+                ee = 0
                 playLetter(B)
+            if event.key == pygame.K_e:
+                ee = ee + 1
+                if ee == 2:
+                    ee = 0
+                    playEE()
+                    
+                
 
 
 
