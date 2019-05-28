2e727ffba5ba95cacc5863469f2873c5167c9c37
bastienTh <bastien.thuet@outlook.fr>
2019-05-28 15:01:10 +0200

    projet = success

--- radar.py
+++ radar.py
@@ -9,29 +9,28 @@ import random
 boot_time = -1000000000
 
 # ---------- Physical button on the rasp ----------
-# import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-# state = 0
-# GPIO_event = 0
-# def button_callback(channel):
-#     global GPIO_event
-#     global state
-#     global boot_time
-#     if boot_time < 0 :
-#        boot_time = time.time()
-#     if GPIO.input(10) == 0:
-#       if state == 1:
-#         print("Button was released!")
-#         GPIO_event = 1
-#       state = 0
-#     else:
-#       if state == 0:
-#         print("Button was pushed!")
-#         GPIO_event = 1  
-#       state = 1
-# GPIO.setwarnings(False) # Ignore warning for now
-# GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-# GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-# GPIO.add_event_detect(10,GPIO.BOTH,callback=button_callback) # Setup event on pin 10 rising edge
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+last_morse = 100000000000
+state = 0
+GPIO_event = 0
+def button_callback(channel):
+    global GPIO_event
+    global state
+    global boot_time
+    if boot_time < 0 :
+       boot_time = time.time()
+    if GPIO.input(10) == 0:
+      if state == 1:
+        GPIO_event = 1
+      state = 0
+    else:
+      if state == 0:
+        GPIO_event = 1  
+      state = 1
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.add_event_detect(10,GPIO.BOTH,callback=button_callback) # Setup event on pin 10 rising edge
 
 # ---------- SOCKET connection with the central table ----------
 import threading
@@ -96,7 +95,7 @@ pygame.mixer.Channel(0).set_endevent()
 pygame.mixer.Channel(1).set_endevent()
 # start playing the background music (white noise at first)
 pygame.mixer.music.load(os.path.join(os.getcwd(), 'sounds', 'whitenoise.wav'))
-pygame.mixer.music.set_volume(0.3)
+pygame.mixer.music.set_volume(0)
 pygame.mixer.music.play(loops=-1)  # loop forever
 
 # the two next functions served to play any letter in the format [0,1,1,0] representing their morse code
@@ -177,9 +176,6 @@ def draw_white_noise(screen):
   screen.fill(BLACK)
   drawed = 0
   while drawed<10000:
-    # r = random.randint(0,255)
-    # width = random.randint(5,10)
-    # height = random.randint(5,10)
     x = int(random.gauss(screen.get_width()/2, screen.get_width()/3))
     y = int(random.gauss(screen.get_height()/2, screen.get_height()/3))
     if (x<0) or (x>screen.get_width()) or (y<0) or (y>screen.get_height()):
@@ -188,11 +184,55 @@ def draw_white_noise(screen):
       pygame.draw.rect(screen, WHITE, [x, y, 4, 6], 0)
       drawed = drawed+1
     
+installation = True
+# game set mode
+automatic_start = 5
+while installation:
+    screen.fill(BLACK)
+    
+    title_font = pygame.font.SysFont("freeserif", 60)
+    regular_font = pygame.font.SysFont("freeserif", 30)
+    
+    title = title_font.render("Mise en place de l'énigme", True,WHITE)
+    text1 = regular_font.render("Démarrer l'énigme : rester appuyé 3 secondes sur le bouton --> !!! Attention: son horrible dans le casque !!!", True, WHITE)
+    text2 = regular_font.render("L'énigme démarre brouillée et se déverrouille X min après le lancement de celle-ci", True, WHITE)
+    textconseil2 = regular_font.render("Idéalement, le jeu doit se déverrouiller entre 5 et 10 min après l'entrée des participants.", True, WHITE)
+    textconseil1 = regular_font.render("Nous laissons au soins du préparateur de définir ce temps :", True, WHITE)
+    textpreX = regular_font.render("Appui court pour changer (de 5 à 30 min) -> ", True, WHITE)
+    textX = regular_font.render("X = " + str(automatic_start+5) + " min", True, WHITE)
+    text3 = regular_font.render("Débrancher et rebrancher la carte relancera automatiquement le programme du début", True, WHITE)
+    text4 = regular_font.render("Pendant la phase brouillée, si un clavier est branché sur la raspberry pi, un appui sur la touche \"s\" (pour start) déverrouille aussi l'énigme", True, WHITE)
 
-boot_time = time.time()
+    screen.blit(title, (80, 50))
+    screen.blit(text1, (80, 150))
+    screen.blit(text2, (80, 250))
+    screen.blit(textconseil1, (80, 300))
+    screen.blit(textpreX, (80, 350))    
+    screen.blit(textX, (650, 350))
+    screen.blit(textconseil2, (80, 400))
+    screen.blit(text3, (80, 500))
+    screen.blit(text4, (80, 550))
+
+    pygame.display.flip()
+    if GPIO_event == 1:
+      GPIO_event = 0
+      if state == 1:
+        pressed = True
+        last_morse = time.time()
+      elif time.time()-last_morse > 3:
+        installation = False
+      else:
+        last_morse = 1000000000000000
+        automatic_start = (automatic_start+1)%26
+    if time.time()-last_morse > 3.1:
+        installation = False
+automatic_start = automatic_start+5
+
 # ------------ Bugged mode ---------------------
+pygame.mixer.music.set_volume(0.2)
+boot_time = time.time()
 while not unlocked:
-  if(time.time()-boot_time < 0):
+  if time.time()-boot_time > 60 * automatic_start:
     unlocked = True
   for event in pygame.event.get():
     if event.type == pygame.KEYDOWN:
@@ -203,29 +243,86 @@ while not unlocked:
   pygame.display.flip()
 
 # ------------- Rebooting ------------------------
-# for i in range(0,5)
-#   screen.fill(BLACK)
-#   font = pygame.font.SysFont("freeserif", 30)
-#   text = font.render("Rebooting ...", True, GREEN)
-#   screen.blit(text, (20, 20))
-#   pygame.display.flip()
-#   time.sleep(2)
-#   text = font.render("Rebooting ....", True, GREEN)
-#   screen.blit(text, (20, 20))
-#   pygame.display.flip()
-#   time.sleep(2)
-#   text = font.render("Rebooting .....", True, GREEN)
-#   screen.blit(text, (20, 20))
-#   pygame.display.flip()
-#   time.sleep(2)
-#   text = font.render("Rebooting ......", True, GREEN)
-#   screen.blit(text, (20, 20))
-#   pygame.display.flip()
-#   time.sleep(2)
-#   text = font.render("Rebooting .......", True, GREEN)
-#   screen.blit(text, (20, 20))
-#   pygame.display.flip()
-#   time.sleep(2)
+pygame.mixer.music.set_volume(0)
+screen.fill(BLACK)
+font = pygame.font.SysFont("freeserif", 30)
+for i in range(0,5):
+  screen.fill(BLACK)
+  text = font.render("Rebooting .", True, GREEN)
+  screen.blit(text, (20, 20))
+  pygame.display.flip()
+  time.sleep(1)
+  
+  screen.fill(BLACK)
+  text = font.render("Rebooting ..", True, GREEN)
+  screen.blit(text, (20, 20))
+  pygame.display.flip()
+  time.sleep(1)
+  
+  screen.fill(BLACK)
+  text = font.render("Rebooting ...", True, GREEN)
+  screen.blit(text, (20, 20))
+  pygame.display.flip()
+  time.sleep(1)
+  
+  screen.fill(BLACK)
+  text = font.render("Rebooting ....", True, GREEN)
+  screen.blit(text, (20, 20))
+  pygame.display.flip()
+  time.sleep(1)
+  
+  screen.fill(BLACK)
+  text = font.render("Rebooting .....", True, GREEN)
+  screen.blit(text, (20, 20))
+  pygame.display.flip()
+  time.sleep(1)
+  
+# ------------- Starting animation ------------------
+rect_offset = 10
+
+screen.fill(BLACK)
+pygame.display.flip()
+
+pygame.draw.rect(screen, GREEN, [tlcorner[0]-border_width, tlcorner[1]-border_width, brcorner[0]-(tlcorner[0]-2*border_width), brcorner[1]-(tlcorner[1]-2*border_width)], 2)
+pygame.display.flip()
+time.sleep(0.5)
+
+pygame.draw.rect(screen, GREEN, [tlcorner[0]+to_tuch_the_circle, tlcorner[1]+to_tuch_the_circle, brcorner[0]-(tlcorner[0]+2*to_tuch_the_circle), brcorner[1]-(tlcorner[1]+2*to_tuch_the_circle)], 2)
+pygame.display.flip()
+time.sleep(0.1)
+
+pygame.draw.circle(screen, GREEN, tlcorner, border_width, 2)
+pygame.display.flip()
+time.sleep(0.5)
+
+pygame.draw.circle(screen, GREEN, trcorner, border_width, 2)
+pygame.display.flip()
+time.sleep(1)
+
+pygame.draw.circle(screen, GREEN, blcorner, border_width, 2)
+pygame.display.flip()
+time.sleep(0.2)
+
+pygame.draw.circle(screen, GREEN, brcorner, border_width, 2)
+pygame.display.flip()
+time.sleep(1)
+
+pygame.draw.rect(screen, GREEN, [morse_window[0]-rect_offset, morse_window[1]-rect_offset, morse_window[2] - morse_window[0] + 2*rect_offset, morse_window[3] - morse_window[1] + 2*rect_offset], 2)
+pygame.display.flip()
+time.sleep(0.1)
+
+pygame.draw.circle(screen, GREEN, radar_center, radar_radius, 2)
+pygame.display.flip()
+time.sleep(0.1)
+
+pygame.draw.circle(screen, GREEN, radar_center, int(radar_radius*0.7), 2)
+pygame.display.flip()
+time.sleep(0.1)
+
+pygame.draw.circle(screen, GREEN, radar_center, int(radar_radius*0.4), 2)
+pygame.display.flip()
+time.sleep(3)
+  
 
 # ------------- Regular mode ---------------------
 # start playing the background music
@@ -243,20 +340,26 @@ pygame.draw.circle(screen, GREEN, brcorner, border_width, 2)
 
 
 done = False
-last_morse = 100000000000
-jump = 0
+jump = 2
 count = 0
 while not done:
-  # if GPIO_event == 1:
-  #   GPIO_event = 0
-  #   if state == 1:
-  #     x_speed = 1
-  #     color = GREEN
-  #   else:
-  #     color = BLACK   
+  if GPIO_event == 1:
+    GPIO_event = 0
+    if state == 1:
+      last_morse = 100000000000
+      jump = 0
+      x_speed = 1
+      color = GREEN
+    else:
+      last_morse = time.time()
+      x_speed = 0
+      color = BLACK
+      pygame.draw.rect(screen, BLACK, [x_coord, y_coord+7, 5, 2], 0)
+      x_coord = x_coord + 10
   for event in pygame.event.get():
     if event.type == pygame.KEYDOWN:
       if event.key == pygame.K_ESCAPE:
+        print("ESC key pressed --> game stopped")
         done = True
       if event.key == pygame.K_a:
           last_morse = time.time()
@@ -272,18 +375,24 @@ while not done:
         
 
   # ------------- MORSE code ---------------
-  rect_offset = 10
   pygame.draw.rect(screen, GREEN, [morse_window[0]-rect_offset, morse_window[1]-rect_offset, morse_window[2] - morse_window[0] + 2*rect_offset, morse_window[3] - morse_window[1] + 2*rect_offset], 2)
   # Move the object according to the speed vector.
   x_coord = x_coord + x_speed
   y_coord = y_coord + y_speed
-  print(time.time()-last_morse)
-  if time.time()-last_morse > 3.5 and jump != 2:
+  
+  if jump == 0 and (time.time()-last_morse) > 1.8:
     jump = jump + 1
     last_morse = time.time()
     pygame.draw.rect(screen, BLACK, [x_coord-1, y_coord+7, 6, 2], 0) 
+    x_coord = morse_window[0] 
     y_coord += 20
-  if x_coord > morse_window[2]:
+  elif jump == 1 and (time.time()-last_morse) > 4:
+    jump = jump + 1
+    last_morse = time.time()
+    pygame.draw.rect(screen, BLACK, [x_coord-1, y_coord+7, 6, 2], 0) 
+    x_coord = morse_window[0] 
+    y_coord += 20
+  elif x_coord > morse_window[2]:
     pygame.draw.rect(screen, BLACK, [x_coord-1, y_coord+7, 6, 2], 0) 
     y_coord += 20
     if y_coord > morse_window[3]:
@@ -325,5 +434,5 @@ while not done:
   draw_radar_point(screen, radar_center, radar_radius*dist_ratio, angle, 1, GREEN)
   pygame.display.flip()
 
-# GPIO.cleanup() # Clean up
+GPIO.cleanup() # Clean up
 pygame.quit()
